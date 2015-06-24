1be9c5ca01c635c5e41933d3024246de6bea2e06
Matt Wizeman <matt.wizeman@gmail.com>
2015-06-24 14:56:11 -0400

    updated to point at the archive url for accumulo 1.6

--- docker/dev/scripts/install-accumulo.sh
+++ docker/dev/scripts/install-accumulo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-wget -O /opt/accumulo-1.6.1-bin.tar.gz http://www.us.apache.org/dist/accumulo/1.6.1/accumulo-1.6.1-bin.tar.gz
+wget -O /opt/accumulo-1.6.1-bin.tar.gz http://archive.apache.org/dist/accumulo/1.6.1/accumulo-1.6.1-bin.tar.gz
 tar -xzf /opt/accumulo-1.6.1-bin.tar.gz -C /opt
 rm /opt/accumulo-1.6.1-bin.tar.gz
 ln -s /opt/accumulo-1.6.1 /opt/accumulo
