a6081da011e2b114e2dd318847a128a00224fbc0
Jason Harwig <jason@pinepointsoftware.com>
2015-06-30 12:41:57 -0500

    Fix filesize not formatting correctly in diff panel. #183

--- web/war/src/main/webapp/js/util/vertex/formatters.js
+++ web/war/src/main/webapp/js/util/vertex/formatters.js
@@ -459,6 +459,7 @@ define([
                     switch (ontologyProperty.displayType) {
                         case 'phoneNumber': return F.string.phoneNumber(value);
                         case 'ssn': return F.string.ssn(value);
+                        case 'byte':
                         case 'bytes': return F.bytes.pretty(value);
                         case 'heading': return F.number.heading(value);
                         case 'duration' : return F.number.duration(value);
