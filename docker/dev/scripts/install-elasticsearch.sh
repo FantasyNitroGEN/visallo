8c87c4cd8edb2bf922b7c339f655c6cffb4e8c8b
Joe Ferner <joe@fernsroth.com>
2015-07-22 13:58:16 -0400

    upgrade ES from 1.4.4 in the dev docker image to 1.7.0

--- docker/dev/scripts/install-elasticsearch.sh
+++ docker/dev/scripts/install-elasticsearch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eu
 
-wget -O /opt/elasticsearch-1.4.4.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz
-tar -xzf /opt/elasticsearch-1.4.4.tar.gz -C /opt
-rm /opt/elasticsearch-1.4.4.tar.gz
-ln -s /opt/elasticsearch-1.4.4 /opt/elasticsearch
-rm -rf /opt/elasticsearch-1.4.4/logs
+wget -O /opt/elasticsearch-1.7.0.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.0.tar.gz
+tar -xzf /opt/elasticsearch-1.7.0.tar.gz -C /opt
+rm /opt/elasticsearch-1.7.0.tar.gz
+ln -s /opt/elasticsearch-1.7.0 /opt/elasticsearch
+rm -rf /opt/elasticsearch-1.7.0/logs
 mkdir -p /var/log/elasticsearch
-ln -s /var/log/elasticsearch /opt/elasticsearch-1.4.4/logs
+ln -s /var/log/elasticsearch /opt/elasticsearch-1.7.0/logs
 /opt/elasticsearch/bin/plugin -install mobz/elasticsearch-head
-/opt/elasticsearch/bin/plugin -install org.vertexium/vertexium-elasticsearch-plugin/0.10.0
+/opt/elasticsearch/bin/plugin -install org.vertexium/vertexium-elasticsearch-plugin/1.0.2
