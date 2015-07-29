90334a28217fadf8af14e05ed93b13268e96a0e7
Joe Ferner <joe@fernsroth.com>
2015-07-29 14:16:23 -0400

    fix java 1.7 compile error

--- core/core/src/main/java/org/visallo/core/trace/TracedMethodInterceptor.java
+++ core/core/src/main/java/org/visallo/core/trace/TracedMethodInterceptor.java
@@ -3,23 +3,15 @@ package org.visallo.core.trace;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
-import java.lang.reflect.Parameter;
-
 public class TracedMethodInterceptor implements MethodInterceptor {
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         if (Trace.isEnabled()) {
             String description = invocation.getMethod().getDeclaringClass().getSimpleName() + "." + invocation.getMethod().getName();
             try (TraceSpan trace = Trace.start(description)) {
-                Parameter[] methodParameters = invocation.getMethod().getParameters();
                 Object[] invocationArguments = invocation.getArguments();
-                int i;
-                String argumentName = "";
-                for (i = 0; i < methodParameters.length; i++) {
-                    argumentName = methodParameters[i].getName();
-                    addArgumentToData(trace, argumentName, invocationArguments[i]);
-                }
-                for (; i < invocationArguments.length; i++) {
+                for (int i = 0; i < invocationArguments.length; i++) {
+                    String argumentName = "arg" + i;
                     addArgumentToData(trace, argumentName, invocationArguments[i]);
                 }
                 return invocation.proceed();
