a5b958deceb300514e74a8a4ebaf6d1cb1250cde
Joe Ferner <joe@fernsroth.com>
2015-08-04 11:44:03 -0400

    add serialVersionUID

--- core/core/src/main/java/org/visallo/core/exception/VisalloException.java
+++ core/core/src/main/java/org/visallo/core/exception/VisalloException.java
@@ -1,6 +1,8 @@
 package org.visallo.core.exception;
 
 public class VisalloException extends RuntimeException {
+    private static final long serialVersionUID = -4322348262201847859L;
+
     public VisalloException(String message) {
         super(message);
     }
