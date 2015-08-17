18463cc9654571148002b9847a9451705df2603b
David Singley <dsingley@vt.edu>
2015-08-17 13:50:20 -0400

    cleanup and color

--- bin/setup-osx-env.sh
+++ bin/setup-osx-env.sh
@@ -2,24 +2,34 @@
 
 DIR=$(cd $(dirname "$0") && pwd)
 
-usage()
-{
-    echo ""
-    echo "----------------------------------------------------------"
-    echo "Mac OSX Visallo Development Environment Setup Instructions"
-    echo "----------------------------------------------------------"
-    echo ""
-    echo "1) Download and install the latest Java 8 SE Development Kit from http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html"
-    echo "2) Download and install docker according to the instructions found at https://docs.docker.com/installation/mac/ but do not follow any of the installer's instructions post installation."
-    echo "3) Run this script."
-    echo ""
-    exit 1
+function _usage {
+  local error_message=${*:-}
+  if [ "${error_message}" ]; then
+    echo >&2
+    echo >&2 $'\e[01;31mERROR:\e[00;00m' ${error_message}
+  fi
+  echo >&2
+  echo >&2 '----------------------------------------------------------'
+  echo >&2 $'Mac OSX \e[01;31mVisallo\e[00;00m Development Environment Setup Instructions'
+  echo >&2 '----------------------------------------------------------'
+  echo >&2
+  echo >&2 '1) Download and install the latest Java 8 SE Development Kit from http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html'
+  echo >&2 '2) Download and install Docker according to the instructions found at https://docs.docker.com/installation/mac/'
+  echo >&2 '     DO NOT run the Docker installer post installation instructions!'
+  echo >&2 '3) Run this script'
+  echo >&2
+  exit 1
 }
 
-type javac >/dev/null 2>&1 || { echo >&2 "javac compiler for Java was not found. Please see the installation instructions below for more information."; usage; }
-type docker-machine >/dev/null 2>&1 || { echo >&2 "docker-machine was not found. If you've already installed docker-machine, make sure it is in your PATH. Please see the installation instructions below for more information."; usage; }
+function _message {
+  local message="$*"
+  echo $'\e[01;31m>>>\e[00;00m' ${message}
+}
 
-echo ">> Creating docker-machine VM. This may take a while."
+type javac &>/dev/null || _usage 'Java compiler (javac) not found in your PATH'
+type docker-machine &>/dev/null || _usage 'docker-machine not found in your PATH'
+
+_message 'Creating Docker virtial machine'
 docker-machine create \
   --driver virtualbox \
   --virtualbox-memory 8192 \
@@ -28,16 +38,13 @@ docker-machine create \
 
 eval "$(docker-machine env visallo-dev)"
 
-echo ">> Configuring docker-machine VM"
+_message 'Configuring Docker virtual machine'
 vm_ip=$(docker-machine ssh visallo-dev ip addr show eth1 | awk '/inet / {print $2}' | awk -F / '{print $1}')
 docker-machine ssh visallo-dev "sudo sed -i -e 's/visallo-dev localhost/localhost/' -e '2i ${vm_ip} visallo-dev' /etc/hosts"
 
+_message 'Building the Visallo development Docker image'
 $(cd ${DIR}/../docker; pwd)/build-dev.sh
 
-echo ">> Building the development Docker container"
-RUN_SCRIPT_PATH=$(cd ${DIR}/../docker; pwd)/run-dev.sh
-
-echo ""
-echo ">> You can now start your development environment container by running the following commands."
-echo '>> 1) eval "$(docker-machine env visallo-dev)"'
-echo ">> 2) $RUN_SCRIPT_PATH"
+_message 'You can now start your Visallo development environment Docker container by running the following commands:'
+_message '1) eval $(docker-machine env visallo-dev)'
+_message "2) $(cd ${DIR}/../docker; pwd)/run-dev.sh"
