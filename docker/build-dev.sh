02b375b178137e888e49ea8febb6978b0c5c89cd
Joe Ferner <joe@fernsroth.com>
2015-08-17 16:24:32 -0400

    don't use sudo if docker is configuigured to not need sudo

--- docker/build-dev.sh
+++ docker/build-dev.sh
@@ -3,9 +3,20 @@
 DIR=$(cd $(dirname "$0") && pwd)
 cd ${DIR}
 
+if [ $(uname) = 'Linux' ]; then
+  docker ps >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    SUDO=sudo
+  else
+    SUDO=
+  fi
+else
+  SUDO=
+fi
+
 case $(uname) in
   Linux)
-    (cd ${DIR} && sudo docker build -t visallo/dev dev)
+    (cd ${DIR} && ${SUDO} docker build -t visallo/dev dev)
     ;;
   Darwin)
     (cd ${DIR} && docker build -t visallo/dev dev)
