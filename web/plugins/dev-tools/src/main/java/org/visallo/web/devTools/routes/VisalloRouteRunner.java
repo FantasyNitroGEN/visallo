55cee606354472054cacb2d35fb60b086fc5fb54
Joe Ferner <joe@fernsroth.com>
2015-08-21 14:57:25 -0400

    add csrf token and workspace id to VisalloRouteRunner

--- web/plugins/dev-tools/src/main/java/org/visallo/web/devTools/routes/VisalloRouteRunner.java
+++ web/plugins/dev-tools/src/main/java/org/visallo/web/devTools/routes/VisalloRouteRunner.java
@@ -1,10 +1,25 @@
 package org.visallo.web.devTools.routes;
 
 import com.v5analytics.webster.handlers.RouteRunner;
+import org.apache.commons.io.IOUtils;
+import org.visallo.core.exception.VisalloException;
 
 public class VisalloRouteRunner extends RouteRunner {
+    private String additionalJavascript;
+
     @Override
     protected String getPageTitle() {
         return "Visallo";
+    }
+
+    protected String getAdditionalJavascript() {
+        if (additionalJavascript == null) {
+            try {
+                additionalJavascript = IOUtils.toString(VisalloRouteRunner.class.getResourceAsStream("visalloRouteRunner.js"));
+            } catch (Exception e) {
+                throw new VisalloException("Could not load visalloRouteRunner.js", e);
+            }
+        }
+        return additionalJavascript;
     }
 }
