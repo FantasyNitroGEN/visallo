68d78b2d36aa54f3bbd4a0622100f778e9bf9d6b
Steve Farley <steve@v5analytics.com>
2015-08-21 16:17:06 -0400

    set ZOO_LOG_DIR for dev docker

--- docker/dev/scripts/install-zookeeper.sh
+++ docker/dev/scripts/install-zookeeper.sh
@@ -6,3 +6,4 @@ rm /opt/zookeeper-3.4.6.tar.gz
 ln -s /opt/zookeeper-3.4.6 /opt/zookeeper
 cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg
 mkdir -p /tmp/zookeeper
+mkdir -p /var/log/zookeeper
