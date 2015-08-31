0de5103ad0186f8accbb0b840f4954a693f036e3
Steve Farley <steve@v5analytics.com>
2015-08-31 13:36:17 -0400

    removed persistent mapping for rabbitmq - it's not working on windows

--- docker/run-dev.sh
+++ docker/run-dev.sh
@@ -94,7 +94,6 @@ ${SUDO} cp ${DIR}/../config/log4j.xml ${LOCAL_PERSISTENT_DIR}/opt/visallo/config
   -v ${PERSISTENT_DIR}/var/lib/hadoop-hdfs:/var/lib/hadoop-hdfs \
   -v ${PERSISTENT_DIR}/var/local/hadoop:/var/local/hadoop \
   -v ${PERSISTENT_DIR}/opt/elasticsearch/data:/opt/elasticsearch/data \
-  -v ${PERSISTENT_DIR}/opt/rabbitmq/var:/opt/rabbitmq/var \
   -v ${MVN_REPO}:/opt/maven/repository \
   -v ${LOCAL_PERSISTENT_DIR}/opt/visallo:/opt/visallo \
   -v ${LOCAL_PERSISTENT_DIR}/opt/jetty/webapps:/opt/jetty/webapps \
