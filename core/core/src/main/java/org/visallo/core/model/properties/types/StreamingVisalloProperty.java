64e6c933b3e403301c674edb6d81c5f7d1947cca
Joe Ferner <joe@fernsroth.com>
2015-09-02 14:32:31 -0400

    add helper methods to get string and byte[] from StreamingVisalloProperty

--- core/core/src/main/java/org/visallo/core/model/properties/types/StreamingVisalloProperty.java
+++ core/core/src/main/java/org/visallo/core/model/properties/types/StreamingVisalloProperty.java
@@ -1,6 +1,11 @@
 package org.visallo.core.model.properties.types;
 
+import org.apache.commons.io.IOUtils;
+import org.vertexium.Vertex;
 import org.vertexium.property.StreamingPropertyValue;
+import org.visallo.core.exception.VisalloException;
+
+import java.io.IOException;
 
 /**
  * An IdentityVisalloProperty for StreamingPropertyValues.
@@ -8,9 +13,34 @@ import org.vertexium.property.StreamingPropertyValue;
 public class StreamingVisalloProperty extends IdentityVisalloProperty<StreamingPropertyValue> {
     /**
      * Create a new StreamingVisalloProperty.
+     *
      * @param key the property key
      */
     public StreamingVisalloProperty(String key) {
         super(key);
+    }
+
+    public byte[] getFirstPropertyValueAsBytes(Vertex vertex) {
+        StreamingPropertyValue propertyValue = getFirstPropertyValue(vertex);
+        if (propertyValue == null) {
+            return null;
+        }
+        try {
+            return IOUtils.toByteArray(propertyValue.getInputStream());
+        } catch (IOException e) {
+            throw new VisalloException("Could not get byte[] from StreamingPropertyValue", e);
+        }
+    }
+
+    public String getFirstPropertyValueAsString(Vertex vertex) {
+        StreamingPropertyValue propertyValue = getFirstPropertyValue(vertex);
+        if (propertyValue == null) {
+            return null;
+        }
+        try {
+            return IOUtils.toString(propertyValue.getInputStream());
+        } catch (IOException e) {
+            throw new VisalloException("Could not get string from StreamingPropertyValue", e);
+        }
     }
 }
