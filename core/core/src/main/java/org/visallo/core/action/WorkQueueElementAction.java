6a36166f9ea65ee4b5882ecec630066fa17d8953
Joe Ferner <joe@fernsroth.com>
2015-09-22 14:13:45 -0400

    add createWorkflowData

--- core/core/src/main/java/org/visallo/core/action/WorkQueueElementAction.java
+++ core/core/src/main/java/org/visallo/core/action/WorkQueueElementAction.java
@@ -1,11 +1,13 @@
 package org.visallo.core.action;
 
 import com.google.inject.Inject;
+import org.json.JSONObject;
 import org.visallo.core.model.workQueue.Priority;
 import org.visallo.core.model.workQueue.WorkQueueRepository;
 import org.visallo.core.user.User;
 
 public class WorkQueueElementAction extends Action {
+    public static final String PROPERTY_PRIORITY = "priority";
     private final WorkQueueRepository workQueueRepository;
 
     @Inject
@@ -20,7 +22,15 @@ public class WorkQueueElementAction extends Action {
     }
 
     private Priority getPriority(ActionExecuteParameters parameters) {
-        String priorityStr = parameters.getData().optString("priority");
+        String priorityStr = parameters.getData().optString(PROPERTY_PRIORITY);
         return Priority.safeParse(priorityStr);
+    }
+
+    public static JSONObject createWorkflowData(Priority priority) {
+        JSONObject json = Action.createWorkflowData(WorkQueueElementAction.class);
+        if (priority != null) {
+            json.put(PROPERTY_PRIORITY, priority.name());
+        }
+        return json;
     }
 }
