eae53c3fc3aa62236050499f1d63acb6314c239f
Joe Ferner <joe@fernsroth.com>
2015-09-23 08:23:17 -0400

    update elastic search in dev docker image to 1.7.2

--- docker/dev/scripts/install-elasticsearch.sh
+++ docker/dev/scripts/install-elasticsearch.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -eu
 
-wget -O /opt/elasticsearch-1.7.0.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.0.tar.gz
-tar -xzf /opt/elasticsearch-1.7.0.tar.gz -C /opt
-rm /opt/elasticsearch-1.7.0.tar.gz
-ln -s /opt/elasticsearch-1.7.0 /opt/elasticsearch
-rm -rf /opt/elasticsearch-1.7.0/logs
+wget -O /opt/elasticsearch-1.7.2.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz
+tar -xzf /opt/elasticsearch-1.7.2.tar.gz -C /opt
+rm /opt/elasticsearch-1.7.2.tar.gz
+ln -s /opt/elasticsearch-1.7.2 /opt/elasticsearch
+rm -rf /opt/elasticsearch-1.7.2/logs
 mkdir -p /var/log/elasticsearch
-ln -s /var/log/elasticsearch /opt/elasticsearch-1.7.0/logs
+ln -s /var/log/elasticsearch /opt/elasticsearch-1.7.2/logs
 /opt/elasticsearch/bin/plugin -install mobz/elasticsearch-head
-/opt/elasticsearch/bin/plugin -install org.vertexium/vertexium-elasticsearch-plugin/1.0.2
