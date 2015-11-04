b42b2d70c83c18706052cc91816ffc79fe869547
Joe Ferner <joe@fernsroth.com>
2015-11-04 10:14:36 -0500

    add post to HttpRepository

--- core/core/src/main/java/org/visallo/core/http/CachingHttpRepository.java
+++ core/core/src/main/java/org/visallo/core/http/CachingHttpRepository.java
@@ -1,15 +1,16 @@
 package org.visallo.core.http;
 
 import com.google.inject.Inject;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 import org.visallo.core.config.Configuration;
 import org.visallo.core.exception.VisalloException;
 import org.visallo.core.util.VisalloLogger;
 import org.visallo.core.util.VisalloLoggerFactory;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class CachingHttpRepository extends HttpRepository {
     private static final VisalloLogger LOGGER = VisalloLoggerFactory.getLogger(CachingHttpRepository.class);
@@ -31,22 +32,46 @@ public class CachingHttpRepository extends HttpRepository {
     }
 
     @Override
-    public byte[] get(String url) {
-        String urlMd5 = DigestUtils.md5Hex(url);
+    public byte[] get(final String url) {
+        String cacheMd5 = DigestUtils.md5Hex(url);
+        return withCache(url, cacheMd5, new WithCache() {
+            @Override
+            public byte[] doIt() {
+                return CachingHttpRepository.super.get(url);
+            }
+        });
+    }
+
+    @Override
+    public byte[] post(final String url, final List<Parameter> formParameters) {
+        String cacheMd5 = DigestUtils.md5Hex(url + createQueryString(formParameters));
+        return withCache(url, cacheMd5, new WithCache() {
+            @Override
+            public byte[] doIt() {
+                return CachingHttpRepository.super.post(url, formParameters);
+            }
+        });
+    }
+
+    private byte[] withCache(String url, String cacheMd5, WithCache withCache) {
         File indexFile = new File(cacheDir, INDEX_FILE_NAME);
-        File cachedFile = new File(cacheDir, urlMd5);
+        File cachedFile = new File(cacheDir, cacheMd5);
         try {
             if (cachedFile.exists()) {
                 LOGGER.debug("cache hit: %s: %s", url, cachedFile.getAbsolutePath());
                 return FileUtils.readFileToByteArray(cachedFile);
             }
             LOGGER.debug("cache miss: %s: %s", url, cachedFile.getAbsolutePath());
-            byte[] data = super.get(url);
+            byte[] data = withCache.doIt();
             FileUtils.writeByteArrayToFile(cachedFile, data);
-            FileUtils.writeStringToFile(indexFile, urlMd5 + " " + url + "\n", true);
+            FileUtils.writeStringToFile(indexFile, cacheMd5 + " " + url + "\n", true);
             return data;
         } catch (IOException e) {
             throw new VisalloException("Could not read cache file: " + cachedFile.getAbsolutePath(), e);
         }
+    }
+
+    private interface WithCache {
+        byte[] doIt();
     }
 }
--- core/core/src/main/java/org/visallo/core/http/HttpRepository.java
+++ core/core/src/main/java/org/visallo/core/http/HttpRepository.java
@@ -6,12 +6,11 @@ import org.visallo.core.exception.VisalloException;
 import org.visallo.core.util.VisalloLogger;
 import org.visallo.core.util.VisalloLoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PushbackInputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -84,50 +83,59 @@ public abstract class HttpRepository {
 
     public byte[] get(String urlString, int retryCount) {
         try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection;
-            if (proxyType != null) {
-                Proxy proxy = new Proxy(proxyType, proxyAddress);
-                connection = (HttpURLConnection) url.openConnection(proxy);
-                LOGGER.debug("getting (via proxy) %s with retry count %d", urlString, retryCount);
-            } else {
-                connection = (HttpURLConnection) url.openConnection();
-                LOGGER.debug("getting %s with retry count %d", urlString, retryCount);
-            }
-            connection.setRequestProperty("Accept-Encoding", "gzip");
-            int responseCode = connection.getResponseCode();
-            if (responseCode != HttpURLConnection.HTTP_OK) {
-                LOGGER.warn("Failed to get URL: %s", urlString);
-                if (retryCount > 0) {
-                    if (responseCode == HTTP_TOO_MANY_REQUESTS || responseCode == HttpURLConnection.HTTP_FORBIDDEN) {
-                        String rateLimitResetString = connection.getHeaderField("X-RateLimit-Reset");
-                        if (rateLimitResetString != null) {
-                            long rateLimitReset = Long.parseLong(rateLimitResetString);
-                            if (rateLimitReset > 1400000000) {
-                                Date resetDate = new Date(rateLimitReset * 1000);
-                                long millis = resetDate.getTime() - new Date().getTime();
-                                if (millis > 0) {
-                                    LOGGER.info("Hit rate limit (%s). Waiting until %s or %d seconds.", urlString, resetDate.toString(), millis / 1000);
-                                    Thread.sleep(millis);
-                                } else {
-                                    LOGGER.info("Hit rate limit (%s). Retrying.", urlString);
-                                }
-                                return get(urlString, retryCount - 1);
-                            } else {
-                                LOGGER.info("Hit rate limit (%s). Waiting %d seconds.", urlString, rateLimitReset);
-                                Thread.sleep((rateLimitReset + 1) * 1000);
-                                return get(urlString, retryCount - 1);
-                            }
-                        }
-                    }
-                }
-                throw new VisalloException(connection.getResponseMessage() + " (" + responseCode + ")");
-            }
-
-            return IOUtils.toByteArray(getResponseStream(connection));
+            HttpURLConnection connection = openConnection("GET", urlString);
+            return getResponse(connection, urlString, retryCount);
         } catch (Exception ex) {
             throw new VisalloException("Could not get url: " + urlString, ex);
         }
+    }
+
+    private byte[] getResponse(HttpURLConnection connection, String urlString, int retryCount) throws IOException, InterruptedException {
+        int responseCode = connection.getResponseCode();
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            LOGGER.warn("Failed to get URL: %s", urlString);
+            if (retryCount > 0) {
+                if (responseCode == HTTP_TOO_MANY_REQUESTS || responseCode == HttpURLConnection.HTTP_FORBIDDEN) {
+                    String rateLimitResetString = connection.getHeaderField("X-RateLimit-Reset");
+                    if (rateLimitResetString != null) {
+                        long rateLimitReset = Long.parseLong(rateLimitResetString);
+                        if (rateLimitReset > 1400000000) {
+                            Date resetDate = new Date(rateLimitReset * 1000);
+                            long millis = resetDate.getTime() - new Date().getTime();
+                            if (millis > 0) {
+                                LOGGER.info("Hit rate limit (%s). Waiting until %s or %d seconds.", urlString, resetDate.toString(), millis / 1000);
+                                Thread.sleep(millis);
+                            } else {
+                                LOGGER.info("Hit rate limit (%s). Retrying.", urlString);
+                            }
+                            return get(urlString, retryCount - 1);
+                        } else {
+                            LOGGER.info("Hit rate limit (%s). Waiting %d seconds.", urlString, rateLimitReset);
+                            Thread.sleep((rateLimitReset + 1) * 1000);
+                            return get(urlString, retryCount - 1);
+                        }
+                    }
+                }
+            }
+            throw new VisalloException(connection.getResponseMessage() + " (" + responseCode + ")");
+        }
+        return IOUtils.toByteArray(getResponseStream(connection));
+    }
+
+    private HttpURLConnection openConnection(String method, String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection;
+        if (proxyType != null) {
+            Proxy proxy = new Proxy(proxyType, proxyAddress);
+            connection = (HttpURLConnection) url.openConnection(proxy);
+            LOGGER.debug("%s (via proxy) %s", method, urlString);
+        } else {
+            connection = (HttpURLConnection) url.openConnection();
+            LOGGER.debug("%s %s", method, urlString);
+        }
+        connection.setRequestProperty("Accept-Encoding", "gzip");
+        connection.setRequestMethod(method);
+        return connection;
     }
 
     public InputStream getResponseStream(URLConnection connection) throws IOException {
@@ -152,24 +160,57 @@ public abstract class HttpRepository {
     }
 
     public byte[] get(String url, Map<String, String> parameters) {
+        String completeUrl = createUrl(url, Parameter.toList(parameters));
+        return get(completeUrl);
+    }
+
+    public byte[] post(String url, Map<String, String> urlParameters, List<Parameter> formParameters) {
+        String completeUrl = createUrl(url, Parameter.toList(urlParameters));
+        return post(completeUrl, formParameters);
+    }
+
+    public byte[] post(String urlString, List<Parameter> formParameters) {
+        try {
+            byte[] formData = createQueryString(formParameters).getBytes();
+            HttpURLConnection connection = openConnection("POST", urlString);
+            connection.setDoOutput(true);
+            connection.setInstanceFollowRedirects(false);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("charset", "utf-8");
+            connection.setRequestProperty("Content-Length", Integer.toString(formData.length));
+            connection.setUseCaches(false);
+            try (OutputStream out = connection.getOutputStream()) {
+                out.write(formData);
+            }
+            return getResponse(connection, urlString, 0);
+        } catch (Exception ex) {
+            throw new VisalloException("Could not post url: " + urlString, ex);
+        }
+    }
+
+    private String createUrl(String url, List<Parameter> parameters) {
+        String queryString = createQueryString(parameters);
+        return url + "?" + queryString;
+    }
+
+    protected String createQueryString(List<Parameter> parameters) {
         StringBuilder query = new StringBuilder();
         boolean first = true;
-        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+        for (Parameter entry : parameters) {
             if (first) {
                 first = false;
             } else {
                 query.append("&");
             }
-            String urlEncodedValue = null;
+            String urlEncodedValue;
             try {
                 urlEncodedValue = URLEncoder.encode(entry.getValue(), "utf-8");
             } catch (UnsupportedEncodingException e) {
                 throw new VisalloException("Could not find encoder", e);
             }
-            query.append(entry.getKey()).append("=").append(urlEncodedValue);
+            query.append(entry.getName()).append("=").append(urlEncodedValue);
         }
-        String completeUrl = url + "?" + query.toString();
-        return get(completeUrl);
+        return query.toString();
     }
 
     private class ProxyAuthenticator extends Authenticator {
@@ -193,6 +234,32 @@ public abstract class HttpRepository {
             }
             LOGGER.trace("ProxyAuthenticator.getPasswordAuthentication() Ignoring authentication request for: %s:%d", getRequestingHost(), getRequestingPort());
             return null;
+        }
+    }
+
+    public static class Parameter {
+        private final String name;
+        private final String value;
+
+        public Parameter(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public static List<Parameter> toList(Map<String, String> parameters) {
+            List<Parameter> results = new ArrayList<>();
+            for (Map.Entry<String, String> entry : parameters.entrySet()) {
+                results.add(new Parameter(entry.getKey(), entry.getValue()));
+            }
+            return results;
         }
     }
 }
