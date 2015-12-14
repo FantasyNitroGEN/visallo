90f88d8ff6db71b3e53a1b93b360c1ab103257b8
Susan Feng <sfeng88@gmail.com>
2015-12-14 15:38:10 -0500

    change StringMetadataVisalloProperty to extend IdentityMetadataVisalloProperty

--- core/core/src/main/java/org/visallo/core/model/properties/types/StringMetadataVisalloProperty.java
+++ core/core/src/main/java/org/visallo/core/model/properties/types/StringMetadataVisalloProperty.java
@@ -1,27 +1,7 @@
 package org.visallo.core.model.properties.types;
 
-import org.vertexium.Property;
-
-public class StringMetadataVisalloProperty extends MetadataVisalloProperty<String, String> {
+public class StringMetadataVisalloProperty extends IdentityMetadataVisalloProperty<String> {
     public StringMetadataVisalloProperty(final String key) {
         super(key);
-    }
-
-    @Override
-    public String wrap(final String value) {
-        return value;
-    }
-
-    @Override
-    public String unwrap(final Object value) {
-        return value.toString();
-    }
-
-    public static String getValue(Property property) {
-        Object value = property.getValue();
-        if (value == null) {
-            return null;
-        }
-        return value.toString();
     }
 }
