30fcb7a9269afc7e74a4645a0a9f0da9225d0fb0
Joe Ferner <joe@fernsroth.com>
2016-01-13 14:43:40 -0500

    Fix multiple bootstap modal dialogs

--- web/war/src/main/webapp/js/app.js
+++ web/war/src/main/webapp/js/app.js
@@ -123,6 +123,7 @@ define([
                 }
             );
 
+            fixMultipleBootstrapModals();
             document.addEventListener('mousewheel', preventPinchToZoom, true);
             this.on('registerForPositionChanges', this.onRegisterForPositionChanges);
 
@@ -890,4 +891,13 @@ define([
         });
     }
 
+    function fixMultipleBootstrapModals() {
+        $(document).on('show.bs.modal', '.modal', function() {
+            var zIndex = 1040 + (10 * $('.modal:visible').length);
+            $(this).css('z-index', zIndex);
+            setTimeout(function() {
+                $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+            }, 0);
+        });
+    }
 });
