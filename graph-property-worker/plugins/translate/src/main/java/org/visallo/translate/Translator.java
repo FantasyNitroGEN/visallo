55928e7cbe80acaa4506b24d44daad89bc14250b
Steve Farley <steve.farley@v5analytics.com>
2016-02-03 14:31:33 -0500

    removed graph-property-worker/plugins/translate

--- graph-property-worker/plugins/translate/src/main/java/org/visallo/translate/Translator.java
+++
@@ -1,7 +0,0 @@
-package org.visallo.translate;
-
-import org.visallo.core.ingest.graphProperty.GraphPropertyWorkData;
-
-public interface Translator {
-    String translate(String text, String language, GraphPropertyWorkData data);
-}
