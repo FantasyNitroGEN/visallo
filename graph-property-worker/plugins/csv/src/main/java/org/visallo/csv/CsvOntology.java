758a30ae0b6a81d1ba968fccd6720cc82bd5cb7b
Steve Farley <steve.farley@v5analytics.com>
2016-02-03 14:41:07 -0500

    removed graph-property-worker/plugins/csv

--- graph-property-worker/plugins/csv/src/main/java/org/visallo/csv/CsvOntology.java
+++
@@ -1,7 +0,0 @@
-package org.visallo.csv;
-
-import org.visallo.csv.model.MappingProperty;
-
-public class CsvOntology {
-    public static final MappingProperty MAPPING_JSON = new MappingProperty("http://visallo.org/csv#mappingJson");
-}
