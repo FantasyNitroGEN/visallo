2ac7d2bc235241df94aa192d46e555b96695493d
Rajan Pardipuram <rajan.pardipuram@v5analytics.com>
2016-03-07 13:55:27 -0500

    Added default implementation for close().

--- web/web-base/src/main/java/org/visallo/web/initializers/ApplicationBootstrapInitializer.java
+++ web/web-base/src/main/java/org/visallo/web/initializers/ApplicationBootstrapInitializer.java
@@ -2,7 +2,12 @@ package org.visallo.web.initializers;
 
 import javax.servlet.ServletContext;
 import java.io.Closeable;
+import java.io.IOException;
 
 public abstract class ApplicationBootstrapInitializer implements Closeable {
     public abstract void initialize(ServletContext context);
+
+    @Override
+    public void close() throws IOException {
+    }
 }
