ff39a7d417167f28ddd2694d738d95169d8a345f
Jason Harwig <jason@pinepointsoftware.com>
2016-06-24 11:58:59 -0500

    Remove deprecated plugins. Registry is now preferred.

--- web/war/src/main/webapp/js/configuration/plugins/visibility/visibilityEditor.js
+++
@@ -1,8 +0,0 @@
-
-
-define(['util/visibility/edit'], function(VisibilityEditorContainer) {
-    'use strict';
-
-    console.warn('Deprecated visibility path, use util/visibility/edit');
-    return VisibilityEditorContainer;
-});
