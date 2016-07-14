98049ec75406a1ed50ba36c0408679f1dee9ff9e
Jason Harwig <jason@pinepointsoftware.com>
2016-07-14 12:24:40 -0500

    Allow data updaters to have configuration

--- web/war/src/main/webapp/js/data/store.js
+++ web/war/src/main/webapp/js/data/store.js
@@ -9,16 +9,32 @@ define([
         function(e) { return true }
     )
 
-    var store = {},
-        componentsByIdentifier = {},
-        registeredListenersByIdentifier = {},
-        update = _.debounce(updateImmediately, 100),
-        updateChangedKeys = [],
+    var MAX_UPDATE_MILLIS = 100,
+        DEFAULT_NO_CONFIG = 'DEFAULT_NO_CONFIG',
+        _store = {},
+
+        // Throttle calls to update
+        update = _.throttle(updateImmediately, MAX_UPDATE_MILLIS),
+
+        // Component is root level of component tree
+        _componentsByIdentifier = {},
+
+        // What store updaters does this component (and it's descendants) require
+        _updatersRequestedByIdentifier = {},
+
+        // What configuration is being used by updators
+        _configurationValuesByUpdater = {},
+
+        // Changed keys / update loop invocation (cleared after called)
+        _updateChangedKeys = [],
+
         api = {
             _clear: function() {
-                store = {};
-                componentsByIdentifier = {};
-                registeredListenersByIdentifier = {};
+                console.warn('Store clearing is only for testing purposes');
+
+                _store = {};
+                _componentsByIdentifier = {};
+                _updatersRequestedByIdentifier = {};
             },
             execNowAndOnUpdate: function(identifier, callback) {
                 if (!_.isString(identifier) || identifier.length === 0) {
@@ -27,94 +43,162 @@ define([
                 if (!_.isFunction(callback)) {
                     throw new Error('Callback [Function] is required');
                 }
-                if (identifier in componentsByIdentifier) {
+                if (identifier in _componentsByIdentifier) {
                     throw new Error('Identifier must be unique and already used: ' + identifier)
                 }
-                componentsByIdentifier[identifier] = callback;
+                _componentsByIdentifier[identifier] = callback;
                 execCallbackForIdentifier(identifier);
             },
             removeCallback: function(identifier) {
-                delete componentsByIdentifier[identifier];
-            },
-            update: function(key, value) {
-                var trigger = true;
-                if (key in store && _.isEqual(store[key], value)) {
-                    trigger = false;
-                }
-                store[key] = value;
-
-                if (trigger) {
-                    updateChangedKeys.push(key)
-                    update();
-                }
-            },
-            replace: function(replaced) {
-                var trigger = !_.isEqual(replaced, store);
-                store = replaced;
-                if (trigger) {
-                    updateChangedKeys.push(key)
-                    update();
-                }
+                delete _componentsByIdentifier[identifier];
             }
         };
 
     return api;
 
+    function _update(key, value) {
+        var trigger = true;
+        if (key in _store && _.isEqual(_store[key], value)) {
+            trigger = false;
+        }
+        _store[key] = value;
+
+        if (trigger) {
+            _updateChangedKeys.push(key)
+            update();
+        }
+    }
 
     function createRegisterForStoreUpdaters(identifier) {
         return function registerForStoreUpdater(updaterKey, configuration) {
             var updaters = registry.extensionsForPoint('org.visallo.store.updater'),
-                cache = registeredListenersByIdentifier,
-                oldKeys = cache[identifier] || (cache[identifier] = {});
+                configKey = generateKey(updaterKey, configuration),
+                previousUpdaters = _updatersRequestedByIdentifier[identifier] ||
+                    (_updatersRequestedByIdentifier[identifier] = {}),
+                updaterConfigs = previousUpdaters[updaterKey] ||
+                    (previousUpdaters[updaterKey] = {});
 
-            if (updaterKey in oldKeys) {
-                oldKeys[updaterKey]++;
+            if (configKey in updaterConfigs) {
+                updaterConfigs[configKey]++;
             } else {
-                oldKeys[updaterKey] = 1;
+                updaterConfigs[configKey] = 1;
             }
 
-            var updater = _.findWhere(updaters, { identifier: updaterKey })
+            var updater = _.findWhere(updaters, { identifier: updaterKey }),
+                key = keyForUpdaterAndConfig(updaterKey, configKey);
+
             if (updater) {
-                updater.activate(_.partial(api.update, updaterKey));
-            }
-            // TODO: deactivate not needed.
+                _.defer(function() {
+                    updater.activate(_.partial(_update, key), configuration);
+                })
+            } else throw new Error('No store updater found for identifier: ' + updaterKey);
+
+            return createRegisterGetResultFunction(key, updaterKey, configuration)
         }
     }
 
     function createUnregisterForStoreUpdaters(identifier) {
-        return function unregisterForStoreUpdater(updaterKey) {
-            var cache = registeredListenersByIdentifier,
-                oldKeys = cache[identifier] || (cache[identifier] = {});
-
-            if (updaterKey in oldKeys) {
-                oldKeys[updaterKey]--;
+        return function unregisterForStoreUpdater(updaterKey, configuration) {
+            if (arguments.length === 1) {
+                if (_.isFunction(updaterKey)) {
+                    configuration = updaterKey._configuration;
+                    updaterKey = updaterKey._updaterIdentifier;
+                }
+            } else if (arguments.length !== 2) {
+                throw new Error('Either a store updator/configuration combo or a store getter function should be passed');
             }
 
-            if (oldKeys[updaterKey] === 0) {
-                delete oldKeys[updaterKey];
+            var configKey = generateKey(updaterKey, configuration),
+                previousUpdaters = _updatersRequestedByIdentifier[identifier] ||
+                    (_updatersRequestedByIdentifier[identifier] = {}),
+                updaterConfigs = previousUpdaters[updaterKey] ||
+                    (previousUpdaters[updaterKey] = {});
+
+            if (configKey in updaterConfigs) {
+                updaterConfigs[configKey]--;
+            }
+
+            if (updaterConfigs[configKey] === 0) {
+                delete updaterConfigs[configKey];
             }
         }
     }
 
     function execCallbackForIdentifier(identifier) {
-        var listeningForKeys = registeredListenersByIdentifier[identifier],
-            trimmedStore = _.pick(store, _.keys(listeningForKeys));
+        var listeningForKeys = registeredKeysForIdentifier(identifier),
+            trimmedStore = _.pick(_store, listeningForKeys);
 
         trimmedStore.registerForStoreUpdater = createRegisterForStoreUpdaters(identifier);
         trimmedStore.unregisterForStoreUpdater = createUnregisterForStoreUpdaters(identifier);
-        componentsByIdentifier[identifier](trimmedStore);
+        _componentsByIdentifier[identifier](trimmedStore);
     }
 
     function updateImmediately() {
-        _.each(componentsByIdentifier, function(callback, identifier) {
-            var registered = registeredListenersByIdentifier[identifier],
-                registeredKeys = Object.keys(registered),
-                registeredKeyWasChanged = _.intersection(registeredKeys, updateChangedKeys).length;
+        _.each(_componentsByIdentifier, function(callback, identifier) {
+            var registeredKeys = registeredKeysForIdentifier(identifier);
+
+            var registeredKeyWasChanged = _.intersection(registeredKeys, _updateChangedKeys).length;
 
             if (registeredKeyWasChanged) {
                 execCallbackForIdentifier(identifier);
             }
         });
-        updateChangedKeys.length = 0;
+        _updateChangedKeys.length = 0;
     }
+
+    function registeredKeysForIdentifier(identifier) {
+        var registered = _updatersRequestedByIdentifier[identifier],
+            registeredKeys = _.flatten(_.map(registered, function(configs, updaterKey) {
+                return _.map(configs, function(count, configKey) {
+                    return keyForUpdaterAndConfig(updaterKey, configKey);
+                })
+            }), true);
+
+        return _.flatten(registeredKeys)
+    }
+
+    function keyForUpdaterAndConfig(updaterIdentifier, configKey) {
+        return updaterIdentifier + '_' + configKey;
+    }
+
+    function generateKey(updaterKey, configuration) {
+        if (!configuration) return DEFAULT_NO_CONFIG;
+
+        var previousConfigs = (
+                _configurationValuesByUpdater[updaterKey] ||
+                (_configurationValuesByUpdater[updaterKey] = [])
+            ),
+            match = _.find(previousConfigs, function(c) {
+                return _.isEqual(c.configuration, configuration);
+            });
+
+        if (match) {
+            return match.key;
+        }
+
+        var key = ['config', previousConfigs.length].join('_');
+
+        _configurationValuesByUpdater[updaterKey] = {
+            key: key,
+            configuration: configuration
+        }
+
+        return key;
+    }
+
+    function createRegisterGetResultFunction(key, updaterKey, configuration) {
+        var getStoreUpdaterResult = function getStoreUpdaterResult(store) {
+            if (!_.isObject(store)) {
+                throw new Error('Store object must be provided');
+            }
+            return store[key];
+        };
+
+        // Later we can unregister with this function
+        getStoreUpdaterResult._updaterIdentifier = updaterKey;
+        getStoreUpdaterResult._configuration = configuration;
+
+        return getStoreUpdaterResult;
+    }
+
 });
--- web/war/src/main/webapp/test/unit/spec/data/storeTest.js
+++ web/war/src/main/webapp/test/unit/spec/data/storeTest.js
@@ -3,9 +3,12 @@ define([
     'configuration/plugins/registry'
 ], function(store, registry) {
 
-    describe.only('Store', function() {
+    describe('Store', function() {
         beforeEach(function() {
+            var w = console.warn;
+            console.warn = function() { };
             store._clear();
+            console.warn = w;
             this.uuids = [
                 registry.registerExtension('org.visallo.store.updater', {
                     identifier: 'id0',
@@ -36,6 +39,16 @@ define([
                             update('update-but-no-change-value')
                         }, 100)
                     }
+                }),
+                registry.registerExtension('org.visallo.store.updater', {
+                    identifier: 'configuration',
+                    activate: function(update, configuration) {
+                        if (configuration.configValue1 === 'x') {
+                            update('x')
+                        } else if (configuration.configValue1 === 'y') {
+                            update('y')
+                        }
+                    }
                 })
             ];
         })
@@ -48,70 +61,156 @@ define([
         it('should have stable api', function() {
             store.should.have.property('execNowAndOnUpdate').that.is.a('function')
             store.should.have.property('removeCallback').that.is.a('function')
-            store.should.have.property('update').that.is.a('function')
-            store.should.have.property('replace').that.is.a('function')
         })
 
         it('should be able to register new store listeners', done => {
+            var getId0;
             store.execNowAndOnUpdate('test', switchOnCallCount({
                 0: store => {
                     Object.keys(store).length.should.equal(2)
                     store.should.have.property('registerForStoreUpdater')
                     store.should.have.property('unregisterForStoreUpdater')
-                    store.registerForStoreUpdater('id0')
+                    getId0 = store.registerForStoreUpdater('id0')
                 },
                 1: store => {
                     Object.keys(store).length.should.equal(3)
-                    store.should.have.property('id0').that.equals('id0Value')
+                    getId0.should.be.a('function')
+                    getId0(store).should.equal('id0Value')
                 },
                 default: store => {
-                    store.should.have.property('id0').that.equals('id0Value2')
+                    getId0(store).should.equal('id0Value2')
                     done();
                 }
             }))
         })
 
-        it('should do concatenate registration', done => {
+        it('should validate identifier', () => {
+            var fn = store.execNowAndOnUpdate,
+                invalids = ['', {}, [], undefined, null, 0];
+
+            invalids.forEach(function(invalid) {
+                expect(() => { fn(invalid) }).to.throw('Identifier')
+            })
+        })
+
+        it('should validate callback', () => {
+            var fn = store.execNowAndOnUpdate,
+                invalids = ['', {}, [], undefined, null, 0];
+
+            invalids.forEach(function(invalid) {
+                expect(() => { fn('validId', invalid) }).to.throw('Callback')
+            })
+        })
+
+        it('should not reuse identifiers', () => {
+            store.execNowAndOnUpdate('test', function() { })
+
+            expect(function() {
+                store.execNowAndOnUpdate('test', function() { })
+            }, function() { }).to.throw('unique')
+        })
+
+        it('should remove callbacks', (done) => {
+            store.execNowAndOnUpdate('test', function() { })
+            store.removeCallback('test')
+            store.execNowAndOnUpdate('test', () => { done() })
+        })
+
+        it('should throw when requesting non-existing updater', done => {
             store.execNowAndOnUpdate('test', switchOnCallCount({
                 0: store => {
-                    store.registerForStoreUpdater('id0')
-                },
-                1: store => {
-                    store.registerForStoreUpdater('id1')
-                },
-                default: store => {
-                    var keys = Object.keys(store)
-                    keys.indexOf('id0').should.be.at.least(0)
-                    keys.indexOf('id1').should.be.at.least(0)
+                    expect(function() {
+                        store.registerForStoreUpdater('non-existent')
+                    }).to.throw('No store updater found')
                     done()
                 }
             }))
         })
 
-        it('should allow unregistration', done => {
+        it('should do concatenate registration', done => {
+            var getId0, getId1;
             store.execNowAndOnUpdate('test', switchOnCallCount({
                 0: store => {
-                    store.registerForStoreUpdater('id0')
+                    getId0 = store.registerForStoreUpdater('id0')
                 },
                 1: store => {
-                    store.registerForStoreUpdater('id0')
+                    getId1 = store.registerForStoreUpdater('id1')
+                },
+                default: store => {
+                    var keys = Object.keys(store)
+                    getId0(store).should.equal('id0Value2')
+                    getId1(store).should.equal('id1Value')
+                    keys.length.should.equal(4)
+                    done()
+                }
+            }))
+        })
+
+        it('should allow unregistration with getter function', done => {
+            var getId1, getId2;
+            store.execNowAndOnUpdate('test', switchOnCallCount({
+                0: store => {
+                    getId1 = store.registerForStoreUpdater('id1')
+                },
+                1: store => {
+                    Object.keys(store).length.should.equal(3)
+                    getId1(store).should.equal('id1Value')
+                    store.unregisterForStoreUpdater(getId1)
+                    getId2 = store.registerForStoreUpdater('id2')
+                },
+                default: store => {
+                    Object.keys(store).length.should.equal(3)
+                    getId2(store).should.equal('id2Value')
+                    done()
+                }
+            }))
+        })
+        
+        it('should allow unregistration with id', done => {
+            var getId1, getId2;
+            store.execNowAndOnUpdate('test', switchOnCallCount({
+                0: store => {
+                    getId1 = store.registerForStoreUpdater('id1')
+                },
+                1: store => {
+                    Object.keys(store).length.should.equal(3)
+                    getId1(store).should.equal('id1Value')
+                    store.unregisterForStoreUpdater('id1')
+                    getId2 = store.registerForStoreUpdater('id2')
+                },
+                default: store => {
+                    Object.keys(store).length.should.equal(3)
+                    getId2(store).should.equal('id2Value')
+                    done()
+                }
+            }))
+        })
+
+        it('should count unregister calls for identifier', done => {
+            var getId0, getId0_2;
+            store.execNowAndOnUpdate('test', switchOnCallCount({
+                0: store => {
+                    getId0 = store.registerForStoreUpdater('id0')
+                },
+                1: store => {
+                    getId0_2 = store.registerForStoreUpdater('id0')
                     store.unregisterForStoreUpdater('id0')
                 },
                 default: store => {
-                    Object.keys(store).indexOf('id0').should.be.at.least(0)
+                    Object.keys(store).length.should.equal(3)
                     done()
                 }
             }))
         })
 
         it('should only update when key values change', done => {
-            var key = 'update-but-no-change'
+            var getter, key = 'update-but-no-change'
             store.execNowAndOnUpdate('test', switchOnCallCount({
                 0: store => {
-                    store.registerForStoreUpdater(key)
+                    getter = store.registerForStoreUpdater(key)
                 },
                 1: store => {
-                    store[key].should.equal(key + '-value')
+                    getter(store).should.equal(key + '-value')
                     _.delay(function() {
                         done()
                     }, 100)
@@ -123,13 +222,13 @@ define([
         })
 
         it('should only update registered listeners', done => {
-            var spy = chai.spy()
+            var getId0, getId1, getId2, spy = chai.spy()
             store.execNowAndOnUpdate('test1', switchOnCallCount({
                 0: store => {
-                    store.registerForStoreUpdater('id2')
+                    getId2 = store.registerForStoreUpdater('id2')
                 },
                 1: store => {
-                    store.id2.should.equal('id2Value')
+                    getId2(store).should.equal('id2Value')
                     spy()
                 },
                 default: store => {
@@ -138,16 +237,41 @@ define([
             }))
             store.execNowAndOnUpdate('test2', switchOnCallCount({
                 0: store => {
-                    store.registerForStoreUpdater('id0')
-                    store.registerForStoreUpdater('id1')
+                    getId0 = store.registerForStoreUpdater('id0')
+                    getId1 = store.registerForStoreUpdater('id1')
                 },
                 1: store => {
                     spy()
                 },
                 default: store => {
-                    store.id0.should.equal('id0Value2')
+                    getId0(store).should.equal('id0Value2')
                     spy.should.have.been.called.twice
                     done()
+                }
+            }))
+        })
+
+        it('should require configuration for listener', done => {
+            var getTest1, getTest2, d = deferred();
+            store.execNowAndOnUpdate('test1', switchOnCallCount({
+                0: store => {
+                    getTest1 = store.registerForStoreUpdater('configuration', { configValue1: 'x' })
+                    getTest1.should.be.a('function')
+                    expect(getTest1).to.throw('Store')
+                },
+                1: store => {
+                    expect(store.configuration).to.not.exist
+                    getTest1(store).should.equal('x');
+                    d.done();
+                }
+            }))
+            store.execNowAndOnUpdate('test2', switchOnCallCount({
+                0: store => {
+                    getTest2 = store.registerForStoreUpdater('configuration', { configValue1: 'y' })
+                },
+                1: store => {
+                    getTest2(store).should.equal('y')
+                    d.promise.then(done);
                 }
             }))
         })
@@ -159,16 +283,28 @@ define([
         console.log.apply(console, log);
     }
 
+    function deferred() {
+        var f, promise = new Promise(function(_f) {
+            f = _f;
+        })
+        return {
+            done: f,
+            promise: promise
+        };
+    }
+
     function switchOnCallCount(map) {
         var callNumber = 0;
         return function() {
-            var args = _.toArray(arguments);
-            if (callNumber in map) {
-                map[callNumber].apply(null, args);
+            var callNumberStr = String(callNumber++),
+                args = _.toArray(arguments);
+            if (callNumberStr in map) {
+                map[callNumberStr].apply(null, args);
             } else if ('default' in map) {
-                map['default'].apply(null, args);
+                map.default.apply(null, args);
+            } else {
+                throw new Error('No function defined for callnumber', callNumberStr)
             }
-            callNumber++;
         }
     }
 })
