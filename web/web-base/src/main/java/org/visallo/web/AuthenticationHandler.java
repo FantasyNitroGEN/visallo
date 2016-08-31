2f449b5c3847b4d915ae6d974b5b93c9c6329150
Susan Feng <sfeng88@gmail.com>
2016-08-31 15:30:04 -0400

    deprecate getRemoteAddr

--- web/web-base/src/main/java/org/visallo/web/AuthenticationHandler.java
+++ web/web-base/src/main/java/org/visallo/web/AuthenticationHandler.java
@@ -2,6 +2,7 @@ package org.visallo.web;
 
 import com.v5analytics.webster.HandlerChain;
 import com.v5analytics.webster.RequestResponseHandler;
+import org.visallo.web.util.RemoteAddressUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,5 +18,15 @@ public class AuthenticationHandler implements RequestResponseHandler {
         } else {
             response.sendError(HttpServletResponse.SC_FORBIDDEN);
         }
+    }
+
+    /**
+     * @Deprecated
+     *
+     * Use RemoteAddressUtil.getClientIpAddr for future calls to get client IP addresses.
+     */
+    @Deprecated
+    public static String getRemoteAddr(HttpServletRequest request) {
+        return RemoteAddressUtil.getClientIpAddr(request);
     }
 }
