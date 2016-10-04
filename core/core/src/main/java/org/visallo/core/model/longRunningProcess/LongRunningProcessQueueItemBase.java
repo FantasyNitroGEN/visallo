1d8f56646fbaa347e3cbfe0f14f64ab413bd687d
Joe Ferner <joe@fernsroth.com>
2016-10-03 20:05:32 -0400

    Added: Helper method in LongRunningProcessQueueItemBase to convert JSON to Object
    This method aids in creating queue items from JSON

--- core/core/src/main/java/org/visallo/core/model/longRunningProcess/LongRunningProcessQueueItemBase.java
+++ core/core/src/main/java/org/visallo/core/model/longRunningProcess/LongRunningProcessQueueItemBase.java
@@ -1,6 +1,8 @@
 package org.visallo.core.model.longRunningProcess;
 
 import org.json.JSONObject;
+import org.visallo.core.exception.VisalloException;
+import org.visallo.core.util.ClientApiConverter;
 
 public abstract class LongRunningProcessQueueItemBase {
     public String getType() {
@@ -13,5 +15,16 @@ public abstract class LongRunningProcessQueueItemBase {
             return false;
         }
         return clazz.getName().equals(type);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T createFromJson(JSONObject json) {
+        String type = json.getString("type");
+        try {
+            Class<T> clazz = (Class<T>) Class.forName(type);
+            return ClientApiConverter.toClientApi(json, clazz);
+        } catch (ClassNotFoundException ex) {
+            throw new VisalloException("Could not find type: " + type, ex);
+        }
     }
 }
