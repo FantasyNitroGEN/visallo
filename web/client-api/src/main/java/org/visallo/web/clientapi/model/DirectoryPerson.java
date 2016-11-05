f2af7881c394eae98f2a69a49133cb7821cdde44
Jeff Kunkle <kunklejr@gmail.com>
2016-11-05 10:06:13 -0400

    Add email to the list of attributes in the directory person API (#719)

--- web/client-api/src/main/java/org/visallo/web/clientapi/model/DirectoryPerson.java
+++ web/client-api/src/main/java/org/visallo/web/clientapi/model/DirectoryPerson.java
@@ -6,16 +6,32 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @JsonTypeName(DirectoryEntity.TYPE_PERSON)
 public class DirectoryPerson extends DirectoryEntity {
+    private final String email;
+
     @JsonCreator
     public DirectoryPerson(
             @JsonProperty("id") String id,
             @JsonProperty("displayName") String displayName
     ) {
+        this(id, displayName, null);
+    }
+
+    @JsonCreator
+    public DirectoryPerson(
+            @JsonProperty("id") String id,
+            @JsonProperty("displayName") String displayName,
+            @JsonProperty("email") String email
+    ) {
         super(id, displayName);
+        this.email = email;
     }
 
     @Override
     public String getType() {
         return TYPE_PERSON;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
