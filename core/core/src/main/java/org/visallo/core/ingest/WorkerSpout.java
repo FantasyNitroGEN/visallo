1b49ccd967546226984e6ce36611339415a8f099
Joe Ferner <joe@fernsroth.com>
2016-12-05 23:22:35 -0500

    Changed: WorkerBase no longer sleeps in between spout polling
    By removing the sleep and putting the sleep requirement on
    WorkerSpout#nextTuple the overall GPW throughput is increased.

--- core/core/src/main/java/org/visallo/core/ingest/WorkerSpout.java
+++ core/core/src/main/java/org/visallo/core/ingest/WorkerSpout.java
@@ -16,5 +16,13 @@ public abstract class WorkerSpout {
 
     }
 
+    /**
+     * Get the next tuple from the queue. This method should poll and wait for a time period to
+     * prevent spinning and causing high cpu load as the method calling this method will not sleep
+     * if a null is returned. This method should also not wait indefinitely because the calling
+     * method needs to perform periodic work.
+     *
+     * @return null, if no tuple is available in the polling period.
+     */
     public abstract WorkerTuple nextTuple() throws Exception;
 }
