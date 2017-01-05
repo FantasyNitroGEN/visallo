30c5db52b4a824bad7353991054022d0d9ebe8e9
Joe Ferner <joe@fernsroth.com>
2017-01-04 20:20:42 -0500

    Changed: changed worker status to be extensible
    Prior to this commit it was not possible to externally extend the worker
    status objects because it used named json deserialization. By switching
    to class deserialization it is now possible to extend status without
    modifing core classes.

--- core/core/src/main/java/org/visallo/core/status/model/ExternalResourceRunnerStatus.java
+++ core/core/src/main/java/org/visallo/core/status/model/ExternalResourceRunnerStatus.java
@@ -1,12 +1,7 @@
 package org.visallo.core.status.model;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
-
-@JsonTypeName("externalResourceRunner")
 public class ExternalResourceRunnerStatus extends WorkerRunnerStatus {
-    @JsonTypeName("externalResourceWorkerStatus")
     public static class ExternalResourceWorkerStatus extends WorkerStatus {
-
         private String threadName;
 
         public void setThreadName(String threadName) {
--- core/core/src/main/java/org/visallo/core/status/model/GraphPropertyRunnerStatus.java
+++ core/core/src/main/java/org/visallo/core/status/model/GraphPropertyRunnerStatus.java
@@ -1,10 +1,6 @@
 package org.visallo.core.status.model;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
-
-@JsonTypeName("graphPropertyRunner")
 public class GraphPropertyRunnerStatus extends WorkerRunnerStatus {
-    @JsonTypeName("graphPropertyWorkerStatus")
     public static class GraphPropertyWorkerStatus extends WorkerStatus {
 
     }
