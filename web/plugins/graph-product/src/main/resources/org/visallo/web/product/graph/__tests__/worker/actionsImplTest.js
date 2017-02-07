c98749dcc23bc5665db5f8e2e6170ab4691124bc
Evan Oxfeld <evan.oxfeld@v5analytics.com>
2017-02-07 14:18:39 -0500

    Web: Snap to grid graph position fixes
    Fixes issues dropping an element onto the graph when snap to grid is
    enabled and elements snapping back to the current position if not moved
    to a new position on the grid.
    
    Moves position change logic when snap to grid is on out of the reducer
    and into the action creator function to prevent side effects.

---
+++ web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/__tests__/worker/actionsImplTest.js
@@ -0,0 +1,309 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import actions from '../../worker/actions-impl';
+
+jest.mock('data/web-worker/store/actions', () => ({
+    protectFromMain: jest.fn()
+}));
+jest.mock('data/web-worker/util/ajax', () => () =>
+    Promise.resolve({
+        extendedData: {
+            edges: []
+        }
+    }));
+jest.mock('data/web-worker/store/product/actions-impl', () => ({
+    update: jest.fn(() => ({ type: 'MOCK_PRODUCT_UPDATE' }))
+}));
+jest.mock('data/web-worker/store/element/actions-impl', () => ({
+    get: jest.fn(() => ({ type: 'MOCK_ELEMENT_GET' }))
+}));
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+
+describe('graph plugin actions', () => {
+    describe('PRODUCT_GRAPH_SET_POSITIONS', () => {
+        it('should not dispatch if the workspace is read only', () => {
+            const store = mockStore({
+                workspace: {
+                    currentId: 'WORKSPACE1',
+                    byId: {
+                        WORKSPACE1: {
+                            editable: false
+                        }
+                    }
+                },
+                product: {
+                    workspaces: {
+                        WORKSPACE1: {
+                            products: {
+                                PRODUCT1: {}
+                            }
+                        }
+                    }
+                }
+            });
+            expect(
+               store.dispatch(
+                actions.setPositions({
+                    productId: 'PRODUCT1'
+                })
+               )
+            ).toBeUndefined();
+        });
+
+        it('should dispatch PRODUCT_GRAPH_SET_POSITIONS on an existing vertex', () => {
+            const expectedActions = [
+                {
+                    type: 'PRODUCT_GRAPH_SET_POSITIONS',
+                    payload: {
+                        productId: 'PRODUCT1',
+                        workspaceId: 'WORKSPACE1',
+                        updateVertices: {
+                            VERTEX1: { x: 5, y: 10 }
+                        }
+                    }
+                }
+            ];
+            const store = mockStore({
+                workspace: {
+                    currentId: 'WORKSPACE1',
+                    byId: {
+                        WORKSPACE1: {
+                            editable: true
+                        }
+                    }
+                },
+                product: {
+                    workspaces: {
+                        WORKSPACE1: {
+                            products: {
+                                PRODUCT1: {
+                                    extendedData: {
+                                        vertices: {
+                                            0: {
+                                                id: 'VERTEX1',
+                                                pos: {
+                                                    x: 0,
+                                                    y: 0
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            return store.dispatch(
+                actions.setPositions({
+                    productId: 'PRODUCT1',
+                    updateVertices: {
+                        VERTEX1: {
+                            x: 4.5,
+                            y: 10.1
+                        }
+                    }
+                })
+            ).then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+        });
+
+        it('should dispatch PRODUCT_GRAPH_SET_POSITIONS on a new vertex',  () => {
+            const expectedActions = [
+                {
+                    type: 'PRODUCT_GRAPH_SET_POSITIONS',
+                    payload: {
+                        productId: 'PRODUCT1',
+                        workspaceId: 'WORKSPACE1',
+                        updateVertices: {
+                            VERTEX1: { x: 5, y: 10 }
+                        }
+                    }
+                },
+                {
+                    type: 'MOCK_PRODUCT_UPDATE'
+                },
+                {
+                    type: 'MOCK_ELEMENT_GET'
+                }
+            ];
+            const store = mockStore({
+                workspace: {
+                    currentId: 'WORKSPACE1',
+                    byId: {
+                        WORKSPACE1: {
+                            editable: true
+                        }
+                    }
+                },
+                product: {
+                    workspaces: {
+                        WORKSPACE1: {
+                            products: {
+                                PRODUCT1: {
+                                    extendedData: { }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            return store.dispatch(
+                actions.setPositions({
+                    productId: 'PRODUCT1',
+                    updateVertices: {
+                        VERTEX1: {
+                            x: 4.5,
+                            y: 10.1
+                        }
+                    }
+                })
+            ).then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+        });
+
+        it('should dispatch PRODUCT_GRAPH_SET_POSITIONS twice when snap to grid is enabled', () => {
+            const expectedActions = [
+                {
+                    type: 'PRODUCT_GRAPH_SET_POSITIONS',
+                    payload: {
+                        productId: 'PRODUCT1',
+                        workspaceId: 'WORKSPACE1',
+                        updateVertices: {
+                            VERTEX1: { x: 5, y: 10 }
+                        }
+                    }
+                },
+                {
+                    type: 'PRODUCT_GRAPH_SET_POSITIONS',
+                    payload: {
+                        productId: 'PRODUCT1',
+                        workspaceId: 'WORKSPACE1',
+                        updateVertices: {
+                            VERTEX1: { x: 0, y: -12.5 }
+                        }
+                    }
+                }
+            ];
+            const store = mockStore({
+                workspace: {
+                    currentId: 'WORKSPACE1',
+                    byId: {
+                        WORKSPACE1: {
+                            editable: true
+                        }
+                    }
+                },
+                product: {
+                    workspaces: {
+                        WORKSPACE1: {
+                            products: {
+                                PRODUCT1: {
+                                    extendedData: {
+                                        vertices: {
+                                            0: {
+                                                id: 'VERTEX1',
+                                                pos: {
+                                                    x: 0,
+                                                    y: 0
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            return store.dispatch(
+                actions.setPositions({
+                    productId: 'PRODUCT1',
+                    updateVertices: {
+                        VERTEX1: {
+                            x: 4.5,
+                            y: 10.1
+                        }
+                    },
+                    snapToGrid: true
+                })
+            ).then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+        });
+
+        it('should dispatch PRODUCT_GRAPH_SET_POSITIONS twice on a new vertex when snap to grid is enabled',  () => {
+            const expectedActions = [
+                {
+                    type: 'PRODUCT_GRAPH_SET_POSITIONS',
+                    payload: {
+                        productId: 'PRODUCT1',
+                        workspaceId: 'WORKSPACE1',
+                        updateVertices: {
+                            VERTEX1: { x: 5, y: 10 }
+                        }
+                    }
+                },
+                {
+                    type: 'PRODUCT_GRAPH_SET_POSITIONS',
+                    payload: {
+                        productId: 'PRODUCT1',
+                        workspaceId: 'WORKSPACE1',
+                        updateVertices: {
+                            VERTEX1: { x: 0, y: -12.5 }
+                        }
+                    }
+                },
+                {
+                    type: 'MOCK_PRODUCT_UPDATE'
+                },
+                {
+                    type: 'MOCK_ELEMENT_GET'
+                }
+            ];
+            const store = mockStore({
+                workspace: {
+                    currentId: 'WORKSPACE1',
+                    byId: {
+                        WORKSPACE1: {
+                            editable: true
+                        }
+                    }
+                },
+                product: {
+                    workspaces: {
+                        WORKSPACE1: {
+                            products: {
+                                PRODUCT1: {
+                                    extendedData: { }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            return store.dispatch(
+                actions.setPositions({
+                    productId: 'PRODUCT1',
+                    updateVertices: {
+                        VERTEX1: {
+                            x: 4.5,
+                            y: 10.1
+                        }
+                    },
+                    snapToGrid: true
+                })
+            ).then(() => {
+                expect(store.getActions()).toEqual(expectedActions);
+            });
+        });
+    });
+});
