570cc21728b66ce79105962a85c3e252af015a0c
Joey Burke <joeybrk372@users.noreply.github.com>
2017-02-22 13:21:27 -0500

    Upgrade Cytoscape from 2.7.11 to 2.7.14(3.1) (#960)
    * Upgrade Cytoscape from 2.7.11 to 2.7.14
    
    * Issue with requestAnimationFrame in IE which was resolved in 2.7.12
    
    * Wrap cytoscape getCachedImage instead of duplicating function

--- web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/cytoscapeCorsFix.js
+++ web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/cytoscapeCorsFix.js
@@ -1,48 +1,19 @@
 define(['underscore'], function(_) {
+    const warnOnce = _.once((message) => { console.warn(message) });
 
     return function fixCytoscapeCorsHandling(cy) {
         const r = cy.renderer();
         if (_.isFunction(r.getCachedImage)) {
-            r.getCachedImage = fixedGetCachedImage;
+            r.getCachedImage = _.wrap(r.getCachedImage, function(originalFunction, url, crossOrigin, onLoad) {
+                if (_.isObject(crossOrigin) && crossOrigin.value) {
+                    return originalFunction.bind(this)(url, crossOrigin.value, onLoad);
+                } else {
+                    warnOnce('getCachedImage signature has changed, upgrade Cytoscape dependency');
+                    return originalFunction.bind(this)(url, crossOrigin, onLoad);
+                }
+            });
         } else {
-            throw new Error('Expected to replace getCachedImage function');
-        }
-    }
-
-    function fixedGetCachedImage(url, onLoad) {
-        if (arguments.length !== 2) {
-            throw new Error('Expected 2 arguments, maybe cytoscape was upgraded?');
-        }
-        if (!_.isString(url)) {
-            throw new Error('Expected string url argument');
-        }
-        if (!_.isFunction(onLoad)) {
-            throw new Error('Expected function onLoad argument');
-        }
-
-        var r = this;
-        var imageCache = r.imageCache = r.imageCache || {};
-        var cache = imageCache[ url ];
-
-        if( cache ){
-            if( !cache.image.complete ){
-                cache.image.addEventListener('load', onLoad);
-            }
-
-            return cache.image;
-        } else {
-            cache = imageCache[ url ] = imageCache[ url ] || {};
-            var image = cache.image = new Image(); // eslint-disable-line no-undef
-            image.addEventListener('load', onLoad);
-            if ((/^(\/\/|http)/).test(url)) {
-                image.crossOrigin = 'Anonymous'; // prevent tainted canvas
-            }
-            image.onerror = function(e){
-                console.warn('Error loading graph image', e)
-            }
-            image.src = url;
-
-            return image;
+            throw new Error('Expected to wrap getCachedImage function');
         }
     }
 });
