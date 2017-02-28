cafd0f0c2294f4c0c290dc060d81c1bed9f21099
Joey Burke <joeybrk372@users.noreply.github.com>
2017-02-28 18:09:26 -0500

    Add scrolling to tabs in saved search table(master) (#988)
    * Add scroll buttons for tabs
    
    * Move scroll arrows to the right

--- web/plugins/table/src/main/resources/org/visallo/web/table/js/table/Tabs.jsx
+++ web/plugins/table/src/main/resources/org/visallo/web/table/js/table/Tabs.jsx
@@ -1,32 +1,60 @@
 define([
-    'react'
+    'react',
+    './ScrollButtons'
 ], function(
-    React) {
+    React,
+    ScrollButtons) {
     'use strict';
 
-    const Tabs = ({ tabs, activeTab, onTabClick }) => {
-        return (
-            <ul className="tabs">
-                {Object.keys(tabs).map((key) => {
-                    const { count, displayName } = tabs[key];
-                    const tabClass = key === activeTab ? 'active' : '';
+    const Tabs = React.createClass({
+        propTypes: {
+            tabs: React.PropTypes.object.isRequired,
+            activeTab: React.PropTypes.string,
+            onTabClick: React.PropTypes.func
+        },
 
-                    return (
-                        <li onClick={() => onTabClick(key)} key={key} className={tabClass}>
-                            <span className="name">{displayName}</span>
-                            <span className="count">{count}</span>
-                        </li>
-                    );
-                })}
-            </ul>
-        );
-    };
+        getInitialState() {
+            return { offset: 0 }
+        },
 
-    Tabs.propTypes =  {
-        tabs: React.PropTypes.object.isRequired,
-        activeTab: React.PropTypes.string,
-        onTabClick: React.PropTypes.func
-    };
+        componentDidMount() {
+            this.setState({  overflow: this._container.offsetWidth < this._tabs.scrollWidth });
+        },
+
+        onScrollClick(direction) {
+            const { offset } = this.state;
+            const width = this._container.offsetWidth;
+            const totalWidth = this._tabs.scrollWidth;
+            const newOffset = direction === 'left' ? Math.max(offset - width, 0) : Math.min(offset + width, totalWidth - width);
+            this.setState({ offset: newOffset, overflow: totalWidth - newOffset > width });
+        },
+
+        render() {
+            const { offset, overflow } = this.state;
+            const { tabs, activeTab, onTabClick } = this.props;
+
+            return (
+                <div className="tabs">
+                    <div className="list-container" ref={(ref) => {this._container = ref}}>
+                        <ul className="tab-list" ref={(ref) => {this._tabs = ref}} style={{marginLeft: -offset + 'px'}}>
+                            {Object.keys(tabs).map((key) => {
+                                const { count, displayName } = tabs[key];
+                                const tabClass = key === activeTab ? 'active' : '';
+
+                                return (
+                                    <li onClick={() => onTabClick(key)} key={key} className={tabClass}>
+                                        <span className="name">{displayName}</span>
+                                        <span className="count">{count}</span>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <ScrollButtons offset={offset} overflow={overflow} onScrollClick={this.onScrollClick}/>
+                </div>
+            );
+        }
+    });
 
     return Tabs;
 });
