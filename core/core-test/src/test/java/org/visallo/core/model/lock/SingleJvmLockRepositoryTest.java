12426e3902d0a784c2c213b99e0f5bbd20ec2a93
Evan Oxfeld <eoxfeld@gmail.com>
2017-03-06 12:15:12 -0500

    Core: Fix SingleJvmLockRepositoryTest race condition (#1034)

--- core/core-test/src/test/java/org/visallo/core/model/lock/SingleJvmLockRepositoryTest.java
+++ core/core-test/src/test/java/org/visallo/core/model/lock/SingleJvmLockRepositoryTest.java
@@ -3,6 +3,7 @@ package org.visallo.core.model.lock;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SingleJvmLockRepositoryTest extends LockRepositoryTestBase {
@@ -13,7 +14,7 @@ public class SingleJvmLockRepositoryTest extends LockRepositoryTestBase {
 
     @Test
     public void testLeaderElection() throws Exception {
-        List<String> messages = new ArrayList<>();
+        List<String> messages = Collections.synchronizedList(new ArrayList<>());
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 2; i++) {
             threads.add(createLeaderElectingThread(lockRepository, "leaderOne", i, messages));
