d073af88ebc744bc8a4ae09eaf6512265311a535
Jason Harwig <jason.harwig@gmail.com>
2017-03-16 12:34:54 -0500

    Flush the cloudfront cache on deploy (#1073)
    We'll up the cache TTL configuration on cloudfront, then use
    invalidations (1000/month free) to invalidate the distribution whenever
    we actually deploy.

--- travis/after-success.sh
+++ travis/after-success.sh
@@ -6,14 +6,21 @@ if [ "$BUILD_DOCS" ]; then
 
   if [ -d "docs/_book" ]; then
 
+    # Cloudfront is in preview for the CLI, enable it
+    aws configure set preview.cloudfront true
+
     if [ "$TRAVIS_BRANCH" = "$VERSION_ROOT" ]; then
       # Copy to root => docs.visallo.org/*
       aws s3 sync docs/_book s3://docs-visallo-com --delete --exclude "versions/*"
       # Copy to versioned => docs.visallo.org/versions/$BRANCH
       aws s3 sync s3://docs-visallo-com "s3://docs-visallo-com/versions/$TRAVIS_BRANCH" --delete --exclude "versions/*"
+      # Invalidate cloudfront distribution (whole deployment)
+      aws cloudfront create-invalidation --distribution-id E1FQ5XMTOW2BH8 --paths "/*"
     else
       # Copy to versioned => docs.visallo.org/versions/$BRANCH
       aws s3 sync docs/_book "s3://docs-visallo-com/versions/$TRAVIS_BRANCH" --delete
+      # Invalidate cloudfront distribution (just this version)
+      aws cloudfront create-invalidation --distribution-id E1FQ5XMTOW2BH8 --paths "/versions/$TRAVIS_BRANCH/*"
     fi
   fi
 else
