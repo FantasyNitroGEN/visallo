d9f6f4fc3944cbc2f7974f21af2225d0b243d9c8
Joe Ferner <joe@fernsroth.com>
2017-05-06 10:51:08 -0400

    Added: ResourceBundle to FormulaEvaluator.UserContext
    ResourceBundle is often needed when evaluating front end related data.

--- web/web-base/src/main/java/org/visallo/web/routes/workspace/WorkspaceDiff.java
+++ web/web-base/src/main/java/org/visallo/web/routes/workspace/WorkspaceDiff.java
@@ -4,14 +4,12 @@ import com.google.inject.Inject;
 import com.v5analytics.webster.ParameterizedHandler;
 import com.v5analytics.webster.annotations.Handle;
 import org.visallo.core.exception.VisalloResourceNotFoundException;
+import org.visallo.core.formula.FormulaEvaluator;
 import org.visallo.core.model.workspace.Workspace;
 import org.visallo.core.model.workspace.WorkspaceRepository;
 import org.visallo.core.user.User;
 import org.visallo.web.clientapi.model.ClientApiWorkspaceDiff;
 import org.visallo.web.parameterProviders.ActiveWorkspaceId;
-import org.visallo.web.parameterProviders.TimeZone;
-
-import java.util.Locale;
 
 public class WorkspaceDiff implements ParameterizedHandler {
     private final WorkspaceRepository workspaceRepository;
@@ -24,8 +22,7 @@ public class WorkspaceDiff implements ParameterizedHandler {
     @Handle
     public ClientApiWorkspaceDiff handle(
             @ActiveWorkspaceId String workspaceId,
-            Locale locale,
-            @TimeZone String timeZone,
+            FormulaEvaluator.UserContext userContext,
             User user
     ) throws Exception {
         Workspace workspace = workspaceRepository.findById(workspaceId, true, user);
@@ -33,6 +30,6 @@ public class WorkspaceDiff implements ParameterizedHandler {
             throw new VisalloResourceNotFoundException("Cannot find workspace: " + workspaceId);
         }
 
-        return this.workspaceRepository.getDiff(workspace, user, locale, timeZone);
+        return this.workspaceRepository.getDiff(workspace, user, userContext);
     }
 }
