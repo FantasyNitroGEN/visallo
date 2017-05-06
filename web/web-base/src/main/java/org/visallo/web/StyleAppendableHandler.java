a493fc6786ebf495e6eb818c18810dde9bb6afc2
Joe Ferner <joe@fernsroth.com>
2017-05-06 09:49:37 -0400

    Changed: StyleAppendableHandler to compile less on append
    StyleAppendableHandler is only used when devMode is false so we don't need
    to support reloading of less and css. This change compiles and appends the less
    css to a string. This should decrease the time needed to serve up primary css of
    the application since we don't have to compile the less each time.

--- web/web-base/src/main/java/org/visallo/web/StyleAppendableHandler.java
+++ web/web-base/src/main/java/org/visallo/web/StyleAppendableHandler.java
@@ -5,49 +5,51 @@ import com.asual.lesscss.LessOptions;
 import com.v5analytics.webster.HandlerChain;
 import com.v5analytics.webster.RequestResponseHandler;
 import org.apache.commons.io.IOUtils;
-import org.visallo.core.util.VisalloLogger;
-import org.visallo.core.util.VisalloLoggerFactory;
+import org.visallo.core.exception.VisalloException;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class StyleAppendableHandler implements RequestResponseHandler {
-    private static final VisalloLogger LOGGER = VisalloLoggerFactory.getLogger(Router.class);
-
     private LessEngine lessCompiler;
-    private final List<Resource> resources = new ArrayList();
+    private String css = "";
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
         response.setContentType("text/css");
         try (ServletOutputStream out = response.getOutputStream()) {
-            Iterator var5 = resources.iterator();
-
-            while (var5.hasNext()) {
-                Resource resource = (Resource) var5.next();
-                try {
-                    resource.handle(out);
-                    out.write("\n".getBytes());
-                } catch (Exception e) {
-                    LOGGER.error("Unable to process style resource: " + resource.getPath(), e);
-                }
-            }
+            out.write(css.getBytes());
         }
     }
 
-
     public void appendLessResource(String pathInfo) {
-        resources.add(new LessResource(pathInfo));
+        try (InputStream input = this.getClass().getResourceAsStream(pathInfo)) {
+            try (StringWriter writer = new StringWriter()) {
+                IOUtils.copy(input, writer, StandardCharsets.UTF_8);
+                String inputLess = writer.toString();
+                String output = lessCompiler().compile(inputLess);
+                appendCss(output);
+            }
+        } catch (Exception ex) {
+            throw new VisalloException("Could not append less resource: " + pathInfo, ex);
+        }
     }
 
     public void appendCssResource(String pathInfo) {
-        resources.add(new CssResource(pathInfo));
+        try (InputStream in = this.getClass().getResourceAsStream(pathInfo)) {
+            appendCss(IOUtils.toString(in));
+        } catch (IOException ex) {
+            throw new VisalloException("Could not append css resource: " + pathInfo, ex);
+        }
+    }
+
+    private void appendCss(String output) {
+        css += output + "\n";
     }
 
     private synchronized LessEngine lessCompiler() {
@@ -58,56 +60,5 @@ public class StyleAppendableHandler implements RequestResponseHandler {
             lessCompiler = new LessEngine(options);
         }
         return lessCompiler;
-    }
-
-    private interface Resource {
-        public void handle(OutputStream out) throws Exception;
-
-        public String getPath();
-    }
-
-    private class LessResource implements Resource {
-        private String path;
-
-        public LessResource(String path) {
-            this.path = path;
-        }
-
-        @Override
-        public String getPath() {
-            return path;
-        }
-
-        public void handle(OutputStream out) throws Exception {
-            try (InputStream input = this.getClass().getResourceAsStream(path)) {
-                try (StringWriter writer = new StringWriter()) {
-                    IOUtils.copy(input, writer, StandardCharsets.UTF_8);
-                    String inputLess = writer.toString();
-                    String output = lessCompiler().compile(inputLess);
-
-                    PrintWriter outWriter = new PrintWriter(out, true);
-                    outWriter.println(output);
-                }
-            }
-        }
-    }
-
-    private class CssResource implements Resource {
-        private String path;
-
-        public CssResource(String path) {
-            this.path = path;
-        }
-
-        @Override
-        public String getPath() {
-            return path;
-        }
-
-        public void handle(OutputStream out) throws IOException {
-            try (InputStream in = this.getClass().getResourceAsStream(path)) {
-                IOUtils.copy(in, out);
-            }
-        }
     }
 }
